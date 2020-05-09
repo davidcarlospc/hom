9ce459e5d45f49abf06b0a6dcd5da7ec7507cbe3
David Cunha <dcpc@cin.ufpe.br>
2020-05-09 03:05:21 -0300

    Update

--- README.md
+++ README.md
@@ -9,8 +9,15 @@ This script will capture screens from meetings held with researchers from the Ce
 
 # Requirements
 
+- Microsoft Windows OS with PowerShell support
+- "hom" folder form this repository must have been copied to the path c:\exp on your local PC's disk
+- Check if the downloaded folder contains the "img" subfolder (images will be saved here)
 
+# Exectution sequence
 
-
+- Opens a new tab with simet web (measurements start automatically and data is displayed on screen)
+- Wait an estimated fixed time for running the simet web
+- takes a screenshot of the full screen whith the data and names the output file with date and time
+- After a known interval (seconds), perform a pre-established number of repetitions
 
 
--- loop.ps1
+++ loop.ps1
@@ -1,39 +1,46 @@
 #Powershell script to assist the automation process of simet web data collection and other information on screen
-# dp = directory path; ni = number of iterations; it = interval time; st = simet time; rt = remainig time 
 
 # Settings
 $DataPath = "c:\exp"
-$Iterations = 10
+$Iterations = 9
 $IntervalTime = 620
 $SimetTime = 120
 $RemainTime = $IntervalTime - $SimetTime
 $Browser = "msedge"
 cd $DataPath
 
-#Initial messages
-Write-Host -ForegroundColor Yellow "	Measurement interval (in seconds): " $IntervalTime
-Write-Host -ForegroundColor Yellow "	#####################################################"
+#Initial messages and definitions view:
+Write-Host -ForegroundColor white "	Powershell Script Settings (loop.ps1):"
+Write-Host -ForegroundColor gray "	#####################################################"
+Write-Host -ForegroundColor gray "	Measurement interval: " $IntervalTime " seconds"
+Write-Host -ForegroundColor gray "	Number of iterations: " $Iterations
+Write-Host -ForegroundColor gray "	Simet runtime: " $SimetTime " seconds"
+Write-Host -ForegroundColor gray "	Browser: " $Browser
+Write-Host -ForegroundColor gray "	Directory path: " $DataPath
+Write-Host -ForegroundColor gray "	#####################################################"
 
 #Loop start
 for(($x = 1); $x -le $Iterations; $x++)
 {
-Write-Host -ForegroundColor Yellow "	Initializing measurement " $x
+Write-Host -ForegroundColor white "	################################"
+Write-Host -ForegroundColor white "	Initializing measurement " $x
+Write-Host -ForegroundColor white "	################################"
 
 # opens a new tab with simet web (measurements start automatically and data is displayed on screen)
-Write-Host -ForegroundColor Yellow "	Opening the simet web in a new tab in the browser "
+Write-Host -ForegroundColor Yellow "	Opening the simet web in a new tab in the browser..."
 Start-Process -FilePath $Browser -ArgumentList https://beta.simet.nic.br 
 
 # option 1 - the application below simulates the simet web to last the same collection time
 #.\speedtest.exe
 # option 2 - wait an estimated fixed time for running the simet web
-Write-Host -ForegroundColor Yellow "	Running measurement...check your screen organization "
+Write-Host -ForegroundColor Yellow "	Running measurement "$x"...please, check your screen organization for the next screenshot in" $SimetTime "seconds"
 start-sleep -seconds $SimetTime
 
 #the application below takes a screenshot of the two screens whith the data (meet, simet and NetBalancer windows) and names the output file with date and time
 Write-Host -ForegroundColor Yellow "	Saving the screenshot " $x
-.\nircmd.exe savescreenshotfull img\screen0$x$(get-date -f yyyy.MM.dd-hh-mm).png
+.\nircmd.exe savescreenshotfull img\$(get-date -f yyyy.MM.dd-hh_mm)-scr0$x.png
 # Final iteration Message
-Write-Host -ForegroundColor Yellow "	Please, wait  " $RemainTime " seconds..."
+Write-Host -ForegroundColor Yellow "	Another " $RemainTime " seconds until the next measurement..."
 Write-Host -ForegroundColor Yellow "	."
 Write-Host -ForegroundColor Yellow "	.."
 Write-Host -ForegroundColor Yellow "	..."
